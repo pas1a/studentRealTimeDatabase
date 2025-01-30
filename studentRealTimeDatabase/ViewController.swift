e74ef251408ae58f49511b2889fca025024c6662
Johnn <john.gariepy25@gmail.com>
2025-01-30 07:37:44 -0600

    Initial Commit

---
+++ studentRealTimeDatabase/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  studentRealTimeDatabase
+//
+//  Created by JOHN GARIEPY on 1/30/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ studentRealTimeDatabaseTests/studentRealTimeDatabaseTests.swift
@@ -0,0 +1,17 @@
+//
+//  studentRealTimeDatabaseTests.swift
+//  studentRealTimeDatabaseTests
+//
+//  Created by JOHN GARIEPY on 1/30/25.
+//
+
+import Testing
+@testable import studentRealTimeDatabase
+
+struct studentRealTimeDatabaseTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
